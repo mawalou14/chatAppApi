29c543758fa7201f6e4b469e60fc5be8f4384bcf
mawalou14 <awaloum213@gmail.com>
2023-12-25 01:17:11 +0100

    pg connection success

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '6292568',
+      username: 'postgres',
+      entities: [],
+      autoLoadEntities: true,
+      database: 'chatApp',
+      synchronize: true,
     })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {  }
